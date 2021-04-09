21274eb93aced4c4962b2d100e91ae2fd85d8a57
Андрей Стрелков <sonver89.rang@gmail.com>
2021-04-10 00:57:19 +0300

    loop

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int arr[10][10];
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			arr[i][j] = rand() % 2;
+		}
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+
+	return 0;
+}
